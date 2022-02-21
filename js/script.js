5ca5d32725ab2ff0138b502703e241d226758f45
Marcin Lech <arcinech@gmail.com>
2022-02-21 19:19:34 +0100

    Add: ticOrTak function and first event listener

--- js/script.js
+++ js/script.js
@@ -25,6 +25,50 @@
       }
     };
     generateTable();
+
+    //Add event for clicking element;
+    let firstPlayer = 0;
+    let secondPlayer = 1;
+
+    const ticOrTak = function(event) {
+      const elementClassList = event.classList;
+      const row = event.parentNode.rowIndex;
+      const col = event.cellIndex;
+      if (!(elementClassList.contains('played'))) {
+        if (firstPlayer != secondPlayer){
+          elementClassList.add('xmark', 'played');
+          tiktokArray[row][col] = 'X';
+          console.log(tiktokArray[row][col]);
+          firstPlayer=1;
+        } else {
+          elementClassList.add('circle', 'played');
+          tiktokArray[row][col] = 'O';
+          console.log(tiktokArray[row][col]);
+          firstPlayer=0;
+        }
+      } else {
+        console.log('powtórz');
+      }
+    };
+
+    const titleClickHandler = function(event){
+      event.preventDefault();
+      const clickedElement = this;
+      ticOrTok(clickedElement);
+      console.log(`firstPlayer${firstPlayer}`);
+    };
+
+    //call to event listener
+    const tok = document.querySelectorAll('tr');
+
+    for(let tik of tok){
+      const toe = tik.querySelectorAll('td');
+
+      for(let cell of toe){
+        cell.addEventListener('click', titleClickHandler);
+      }
+    }
+
   };
   playGame();
 }
