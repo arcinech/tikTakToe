2945b4481eb0a72a2b2e16ec6f0e5578e4eb867c
Marcin Lech <arcinech@gmail.com>
2022-02-21 22:02:03 +0100

    Add: check win condition and implement it

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-
+'use strickt';
 {
+  const printMessage = function(msg){
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+  };
 
-  const playGame = function(){
+  const clearMessages = function(){
+    document.getElementById('messages').innerHTML = '';
+    document.getElementById('tictoc').innerHTML = '';
+  };
+  const boardGrid = function(){
+    const button = document.getElementById('submitSize');
+
+    const checkBoard = function() {
+      clearMessages();
+      const board = document.getElementById('boardSize').value;
+      if (2 < board < 11){
+        document.getElementById('prompt').classList.add('hidden');
+        document.getElementById('board-title').classList.remove('hidden');
+        playGame(board);
+      } else {
+        printMessage('Wrong size of board');
+      }
+    };
+
+    button.addEventListener('click', checkBoard);
+  };
+  const playGame = function(boardSize){
     let tiktokArray = [[]];
-
-    //create table via js
-    const generateTable = function(){
-      let boardSize = 3;
+    //create table via js3
+    const generateTable = function(boardSize){
       /*while ( boardSize < 3 ){
         boardSize = prompt('Size of tiktok grid (min.3)', boardSize);
       }*/
@@ -24,39 +48,32 @@
         }
       }
     };
-    generateTable();
-    let firstPlayer = 0;
-    let secondPlayer = 1;
+    generateTable(boardSize);
 
+    let firstPlayer = 0;
     const ticOrTak = function(event) {
       const elementClassList = event.classList;
       const row = event.parentNode.rowIndex;
       const col = event.cellIndex;
       if (!(elementClassList.contains('played'))) {
-        if (firstPlayer != secondPlayer){
+        if (firstPlayer == 0){
           elementClassList.add('xmark', 'played');
           tiktokArray[row][col] = 'X';
-          console.log(tiktokArray[row][col]);
-          firstPlayer=1;
+          console.log(tiktokArray);
+          checkWin('Player with X');
+          firstPlayer = 1;
         } else {
           elementClassList.add('circle', 'played');
           tiktokArray[row][col] = 'O';
           console.log(tiktokArray[row][col]);
-          firstPlayer=0;
+          checkWin('Player with O');
+          firstPlayer = 0;3;
         }
       } else {
         console.log('powtórz');
       }
     };
-    const removeListeners = function() {
-      const rows = document.querySelectorAll('tr');
-      for(let row of rows){
-        const cols = row.querySelectorAll('td');
-        for(let col of cols){
-          col.removeEventListener('click');
-        }
-      }
-    };
+
     const addEventListeners = function() {
       const rows = document.querySelectorAll('tr');
 
@@ -68,21 +85,96 @@
         }
       }
     };
+    const removeListeners = function() {
+      const rows = document.querySelectorAll('tr');
+      for(let row of rows){
+        const cols = row.querySelectorAll('td');
+        for(let col of cols){
+          col.removeEventListener('click', titleClickHandler);
+        }
+      }
+    };
     const titleClickHandler = function(event){
       event.preventDefault();
       const clickedElement = this;
       ticOrTak(clickedElement);
       console.log(`firstPlayer${firstPlayer}`);
     };
+
+    const checkHorizontal = function(){
+      console.log(boardSize);
+      for(let row = 0; row < boardSize; row++){
+        let winCondition = 0;
+        for(let col = 0; col < (boardSize-1); col++){
+          /* compare current column with next column */
+          if (tiktokArray[row][col] == tiktokArray[row][col+1] && (tiktokArray[row][col] != '')){
+            console.log(winCondition);
+            winCondition++;
+          }
+          if (winCondition == 2) return true;
+        }
+      }
+      return false;
+    };
+    const checkVertical = function() {
+      for(let col=0; col<boardSize; col++){
+        // console.log("start of loop y" + x);
+        let winCondition = 0;
+        for(let row=0; row < (boardSize - 1); row++){
+          if ((tiktokArray[row][col] == tiktokArray[row+1][col]) && (tiktokArray[row][col] != '')){
+            winCondition++;
+          }
+          if (winCondition == 2) return true;
+        }
+      }
+      return false;
+    };
+    //first diagonal check
+    const checkDiagonal = function() {
+      let winCondition = 0;
+      let col;
+      for(let row=0; row < (boardSize - 1); row++){
+        col = row;
+        if ((tiktokArray[row][col] == tiktokArray[row+1][col+1]) && (tiktokArray[row][col] != '')){
+          winCondition++;
+          console.log(winCondition);
+        }
+        //  console.log(winCondition);
+        if (winCondition == 2) return true;
+      }
+      return false;
+    };
+
+    const checkReversediagonal = function() {
+      let winCondition = 0;
+      let col = 0;
+      for(let row=(boardSize - 1); row > 0; row--){
+        console.log('reversed row: ' + row + ' col: ' + col);
+        if ((tiktokArray[row][col] == tiktokArray[row-1][col+1]) && (tiktokArray[row][col] != '')){
+          winCondition++;
+        }
+        if (winCondition == 2) return true;
+        col++;
+      }
+      return false;
+    };
+    const checkWin = function(player) {
+      console.log('check win run');
+      if (checkHorizontal() == true || checkVertical() == true || checkReversediagonal() == true || checkDiagonal() == true) {
+        removeListeners();
+        printMessage(player);
+        return true;
+      }
+    };
+
     addEventListeners();
-    //call to event listener
   };
 
-  playGame();
-
-
+  boardGrid();
   document.getElementById('restartButton').addEventListener('click', function() {
-    document.getElementById('tictoc').innerHTML = '';
-    playGame();
+    document.getElementById('prompt').classList.remove('hidden');
+    document.getElementById('board-title').classList.add('hidden');
+    clearMessages();
+    boardGrid();
   });
 }
