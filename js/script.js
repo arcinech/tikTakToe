9cdffedabd8bbe86c3204fe577321e680f454f41
Marcin Lech <arcinech@gmail.com>
2022-02-21 18:58:41 +0100

    Remove function other than generateTable

--- js/script.js
+++ js/script.js
@@ -1,59 +1,30 @@
+
 {
-    //create table via js   
+
+  const playGame = function(){
+    let tiktokArray = [[]];
+
+    //create table via js
     const generateTable = function(){
-        let z = 0;
-        while ( z < 3 ){
-            z = prompt("Size of tiktok grid (min.3)", z);
+      let boardSize = 3;
+      /*while ( boardSize < 3 ){
+        boardSize = prompt('Size of tiktok grid (min.3)', boardSize);
+      }*/
+      const tableBody = document.getElementById('tictoc');
+
+      for(let row = 0; row < boardSize; row++){
+
+        const createRow = document.createElement('tr');
+        const rowElement = tableBody.appendChild(createRow);
+        tiktokArray[row] = [];
+        for(let col = 0; col < boardSize; col++){
+          const createCell = document.createElement('td');
+          rowElement.appendChild(createCell).setAttribute('id', row + '-' + col);
+          tiktokArray[row][col] = '';
         }
-        const tableBody = document.getElementById("tictoc");
-
-        for(let x = 0; x < z; x++){
-
-            const row = document.createElement('tr');
-            const createRow = tableBody.appendChild(row);
-            createRow.setAttribute('id',x);
-            for(let y = 0; y < z; y++){
-                const cell = document.createElement('td');
-                createRow.appendChild(cell).setAttribute('id',x + '-' + y);
-            }
-        }
-    }
-    generateTable();
-
-    //Add event for clicking element;
-    let i = 0;
-    const titleClickHandler = function(event){
-        event.preventDefault();
-        const clickedElement = this;
-        if (clickedElement.innerHTML == "") {
-            let j=0;
-            if (i==j){
-                clickedElement.classList.add("X");
-                clickedElement.innerHTML = "X";
-                console.log(clickedElement);
-                i=1;
-            } else {
-                clickedElement.classList.add("y");
-                clickedElement.innerHTML = "y";
-                console.log(clickedElement);
-                i=0;
-        
-            }
-        } else {
-            console.log("powtórz");
-        }
-
+      }
     };
-
-    //call to event listener
-    const tok = document.querySelectorAll("tr");
-
-    for(let tik of tok){
-        const toe = tik.querySelectorAll("td");
-
-        for(let cell of toe){
-            cell.addEventListener('click', titleClickHandler);
-        }
-    }
-
+    generateTable();
+  };
+  playGame();
 }
